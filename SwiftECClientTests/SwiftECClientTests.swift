4a61fae769a48663a0af74b2978491b998b48710
naoto yamaguchi <n.h.in.m.h@gmail.com>
2014-06-10 09:48:47 +0900

    Initial Commit

---
+++ SwiftECClientTests/SwiftECClientTests.swift
@@ -0,0 +1,35 @@
+//
+//  SwiftECClientTests.swift
+//  SwiftECClientTests
+//
+//  Created by naoto yamaguchi on 2014/06/10.
+//  Copyright (c) 2014年 naoto yamaguchi. All rights reserved.
+//
+
+import XCTest
+
+class SwiftECClientTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
